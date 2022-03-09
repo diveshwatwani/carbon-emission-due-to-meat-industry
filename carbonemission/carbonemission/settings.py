d65732c07ccb15ddf887b44368e10acd6394ba88
diveshwatwani <60432327+diveshwatwani@users.noreply.github.com>
2022-03-09 16:42:28 +0530

    v7

--- carbonemission/carbonemission/settings.py
+++ carbonemission/carbonemission/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-8!nm58fux@9*i^qyipg6o!8zur8mv63zd9bp9yxljrpdm&$m@a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
